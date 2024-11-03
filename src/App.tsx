2084757cecda5afa092f0b4db18103f5ee20d746
jkeddo_ <jjkeddo@gmail.com>
2024-11-03 14:52:46 -0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import racecarLogo from "/racecar.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <a href="https://www.youtube.com/watch?v=8fcSviC7cRM" target="_blank">
+          <img src={racecarLogo} className="logo" alt="Vite logo" />
+        </a>
+      </div>
+      <h1>Mock Advisor: A Demo</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">I want to make a difference!</p>
+    </>
+  );
+}
+
+export default App;
