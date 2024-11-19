f03c421d57a39c9583a98a0cc345342496072aab
jkeddo_ <jjkeddo@gmail.com>
2024-11-18 22:59:20 -0800

    DEV-01--Fix-Kidslist-Bug-and-Fix-Dialog-State

--- src/components/KidsMenu/KidsMenu.tsx
+++ src/components/KidsMenu/KidsMenu.tsx
@@ -18,16 +18,30 @@ export const KidsMenu = () => {
     return <></>;
   }
 
-  let kids;
-  try {
-    console.log(`Kids string is the following: ${kidsString}`);
-    kids = JSON.parse(kidsString);
-  } catch (error) {
-    console.warn("Parsing of kids list failed. All children destroyed. Sorry :(");
-    return false;
-  }
-
   const [dialogContent, setDialogContent] = useState(null);
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [kidsList, setKidsList] = useState([]);
+
+  useEffect(() => {
+    const kidsString = localStorage.getItem(KIDS_LIST);
+
+    if (!kidsString) {
+      console.log("No kids set yet");
+      //result false;
+    }
+
+    let kids;
+    try {
+      console.log(`Kids string is the following: ${kidsString}`);
+      kids = JSON.parse(kidsString);
+      setKidsList(kids);
+    } catch (error) {
+      console.warn("Parsing of kids list failed. All children destroyed. Sorry :(");
+      //result false;
+    }
+  }, []);
+
   const dialogRef = useRef<HTMLDialogElement>(null);
 
   function toggleDialog() {
@@ -35,18 +49,39 @@ export const KidsMenu = () => {
       console.log("Problem initializing dialogRef");
       return;
     }
+
+    // Toggle dialog but ensure to reset state first between invocations so that multiple kids can be added.
     dialogRef.current.hasAttribute("open") ? dialogRef.current.close() : dialogRef.current.showModal();
+    setName("");
+    setAge("");
   }
+
+  //   function addKid() {
+  //     if (!name || !age || )
+  //     kids.push()
+  //   }
+
+  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => setName(e.target.value);
+  const handleAgeChange = (e: React.ChangeEvent<HTMLInputElement>) => setAge(e.target.value);
 
   const dialogComponent = (
     <dialog id="addKidModal" ref={dialogRef}>
       <div>Add Kid</div>
+      <span>Name: </span>
+      <input value={name} onChange={handleNameChange} />
+      <span>Age: </span>
+      <input value={String(age)} onChange={handleAgeChange} />
       {dialogContent}
       <button id="addKidSubmit" onClick={toggleDialog}>
         Submit
       </button>
     </dialog>
   );
+
+  if (!kidsString) {
+    console.log("No kids set yet");
+    return <></>;
+  }
 
   return (
     <div>
@@ -59,16 +94,20 @@ export const KidsMenu = () => {
             Add Kid
           </button>
         </div>
-        {kids.map((kid: Kid) => (
+        {kidsList.map((kid: Kid) => (
           <div style={{ float: "left" }} key={kid.name}>
             <span>
               {" "}
               {kid.name}, age {kid.age}
             </span>
             <span> | </span>
-            <span style={{ color: "yellow" }}>edit</span>
+            <span style={{ color: "yellow" }} onClick={() => console.log("edit kid")}>
+              edit
+            </span>
             <span> | </span>
-            <span style={{ color: "red" }}>delete</span>
+            <span style={{ color: "red" }} onClick={() => console.log("delete kid")}>
+              delete
+            </span>
           </div>
         ))}
         {dialogComponent}
